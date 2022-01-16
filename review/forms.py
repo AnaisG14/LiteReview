3af09d745d6c8c5db729e2e27cf92014c7ee0661
AnaisG14 <gatard.anais@gmail.com>
2022-01-16 22:36:10 +0100

    affichage du flux ok par ordre décroissant, toutes les reviews par critique et bouton ajout critique ok

--- review/forms.py
+++ review/forms.py
@@ -1,6 +1,7 @@
 # review/forms
 from django import forms
 from django.contrib.auth import get_user_model
+from django.forms import Textarea, TextInput
 
 from . import models
 from .models import UserFollows
@@ -16,6 +17,24 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = models.Review
         fields = ['headline', 'body', 'rating']
+        RATING_CHOICES = [
+            (0, 0),
+            (1, 1),
+            (2, 2),
+            (3, 3),
+            (4, 4),
+            (5, 5),
+        ]
+        widgets = {
+            'body': Textarea(attrs={'cols': 120, 'rows': 20}),
+            'headline': TextInput(attrs={'size': 100}),
+            'rating': forms.RadioSelect(choices=RATING_CHOICES)
+        }
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['headline'].widget.attrs.update(size='100')
+    #     self.fields['body'].widget.attrs.update(col='800')
 
 
 class FollowUsersForm(forms.ModelForm):
