efcda8fe31301869f9e0f125f9bc4b412f076d47
AnaisG14 <gatard.anais@gmail.com>
2022-06-11 12:09:31 +0200

    fix secret key in production settings

--- litereview/prod_setting.py
+++ litereview/prod_setting.py
@@ -3,6 +3,6 @@ from .settings import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-SECRET_KEY = ' 0gd*i6-jf^@fz7csa-$b)d3r64rurk5pti7%5&5wcj29)_7lds'
+SECRET_KEY = '0gd*i6-jf^@fz7csa-$b)d3r64rurk5pti7%5&5wcj29)_7lds'
 
 ALLOWED_HOSTS = ['litereview.herokuapp.com']
