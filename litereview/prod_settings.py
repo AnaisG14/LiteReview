2ed9adfd45a42ba45149e12a56d731cc7244175f
AnaisG14 <gatard.anais@gmail.com>
2022-06-11 13:14:12 +0200

    setup whitenoise for staticfiles delivery

--- litereview/prod_settings.py
+++ litereview/prod_settings.py
@@ -7,6 +7,9 @@ TEMPLATE_DEBUG = False
 
 DATABASES['default'] = dj_database_url.config()
 
+MIDDLEWARE = ["whitenoise.middleware.WhiteNoiseMiddleware"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 SECRET_KEY = '0gd*i6-jf^@fz7csa-$b)d3r64rurk5pti7%5&5wcj29)_7lds'
 
 ALLOWED_HOSTS = ['litereview.herokuapp.com']
