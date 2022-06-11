27fac662e6a87fec5c18ba80c941005e4b58dffb
AnaisG14 <gatard.anais@gmail.com>
2022-06-11 13:02:41 +0200

    add default database

--- litereview/prod_settings.py
+++ litereview/prod_settings.py
@@ -1,7 +1,11 @@
+import dj_database_url
+
 from .settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = '0gd*i6-jf^@fz7csa-$b)d3r64rurk5pti7%5&5wcj29)_7lds'
 
